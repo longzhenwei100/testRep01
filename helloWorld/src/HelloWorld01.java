f5f13abeea2768c2e5337740fe4517d0086614b6
longzhenwei100 <long%100>
2019-01-06 19:56:14 +0800

    第一次提交分支LONGZHENWEI01

--- helloWorld/src/HelloWorld01.java
+++ helloWorld/src/HelloWorld01.java
@@ -14,10 +14,11 @@ public class HelloWorld01 {
         list.add("你好世界");
         list.add("美丽中国");
         list.add("孙悟空");
-        list.add("唐三彩");
         list.add("沙和尚");
         list.add("猪八戒");
         list.add("唐僧");
+        list.add("观音菩萨");
+        list.add("如来佛");
         return list;
     }
 
